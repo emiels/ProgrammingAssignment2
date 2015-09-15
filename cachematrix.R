dc0162ed61045a1b7692358978fc08cb1b32505c
emiels <emiel.schoenmakers@xs4all.nl>
2015-09-15 18:59:51 +0200

    My solution for Prog Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -18,7 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-## This function computes the inverse of the special "matrix" returned by 
+## This function computes the inverse of the special "matrix" returned by  
 ## `makeCacheMatrix` above. If the inverse has already been calculated (and the 
 ## matrix has not changed), then `cacheSolve` should retrieve the inverse from 
 ## the cache. If matrix has changed (i.e. 'makeCacheMatrix was reapplied) a new
